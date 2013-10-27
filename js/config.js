51eb39efd70f27afe4f225259a2e139dd14bc96b
Daniel Margolis <dmargolis@google.com>
2013-10-28 00:08:28 +0100

    Routing.

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 
 function set_autocomplete() {
-  $('input[name=s]').autocomplete({
+  $('input[name=s], input[name=v], input[name=n]').autocomplete({
     source: function(request, response) {
               $.get('http://transport.opendata.ch/v1/locations', {query: request.term, type: 'station'}, function(data) {
                 response($.map(data.stations, function(station) {
@@ -19,16 +19,24 @@ function set_autocomplete() {
 
 $(function() {
   set_autocomplete();
+  $('#remove_station').hide();
+  $('#remove_route').hide();
 })
 
 function generate_config() {
-  config = {'stations': {} };
+  config = {'stations': [], 'routes':[] };
   var station;
   $('#stations div').each(function() {
     station = $(this).children('input[name=s]')[0].value;
     limit = $(this).children('input[name=l]')[0].value;
-    config.stations[station] = limit;
+    config.stations.push({'station': station, 'limit': limit});
   });
+  $('#routes div').each(function() {
+    von = $(this).children('input[name=v]')[0].value;
+    nach = $(this).children('input[name=n]')[0].value;
+    config.routes.push({'von': von, 'nach': nach});
+  });
+
   return config;
 }
 
@@ -40,16 +48,36 @@ function add_station() {
       '</div>');
   
   set_autocomplete();
-  $('#remove').show();
+  $('#remove_station').show();
 }
 
 function remove_station() {
   var idx = $('#stations>div').length - 1;
   $('#station' + idx).remove();
   if (idx == 1) {
-    $('#remove').hide();
+    $('#remove_station').hide();
   }
 }
+
+function add_route() {
+  var idx = $('#routes>div').length;
+  $('#routes').append('<div id="route' + idx + '"> ' +
+      'Von: <input type="text" name="v" />' +
+      'Nach: <input type="text" name="n" /> ' +
+      '</div>');
+  
+  set_autocomplete();
+  $('#remove_route').show();
+}
+
+function remove_route() {
+  var idx = $('#routes>div').length - 1;
+  $('#route' + idx).remove();
+  if (idx == 1) {
+    $('#remove_route').hide();
+  }
+}
+
 
 function set_configuration() {
   var url = location.protocol + "//" + location.host + "/?config=" + encodeURIComponent(JSON.stringify(generate_config()));
--- js/stationboard.js
+++ js/stationboard.js
@@ -13,21 +13,34 @@
 })(jQuery);
 
 $(function() {
-  
+
   var config = $.QueryString['config'];
   if (config && config.length > 0) {
     config = JSON.parse(config);
+    for (var i = 0; i < config.stations.length; i++) {
+      if (config.stations[i].station == '') {
+        config.stations.splice(i, 1);
+        i--;
+      } 
+    }
+    for (var i = 0; i < config.routes.length; i++) {
+      if (config.routes[i].von == '') {
+        config.routes.splice(i, 1);
+        i--;
+      }
+    }
   } else {
-    config = {'stations':{}};
+    config = {'stations':[], 'routes':[]};
   }
-  var size = 0;
-  for (var station in config.stations) { size++; }
-  if (size == 0) {
-    config.stations['Zürich HB'] = 8;
+  if (config.stations.length == 0 && config.routes.length == 0) {
+    config.stations.push({'station': 'Zürich HB', 'limit': 8});
+    $('#routes').hide();
+  } else if (config.stations.length == 0) {
+    $('#stationboard').hide();
   }
 
-  function get_timetable(station) {
-    $.get('http://transport.opendata.ch/v1/stationboard', {id: station, limit: config.stations[station]}, function(data) {
+  function get_timetable(station, limit) {
+    $.get('http://transport.opendata.ch/v1/stationboard', {id: station, limit: limit}, function(data) {
       $('#' + normalize(station) + ' tbody').empty();
       $(data.stationboard).each(function () {
         var prognosis, departure, delay, line = '<tr><td>';
@@ -46,16 +59,53 @@ $(function() {
       });
       }, 'json');
   }
+  function get_route(from, to) {
+    $.get('http://transport.opendata.ch/v1/connections', {from: from, to: to}, function(data) {
+       var id = '#' + normalize(from) + '-' + normalize(to) + ' tbody';
+       $(id).empty();
+       $(data.connections).each(function() {
+         var departure = moment(this.from.departure).format('HH:mm');
+         var arrival = moment(this.to.arrival).format('HH:mm');
+         var duration = this.duration;
+         var transfers = this.transfers;
+         var route = '';
+         for (var i = 0; i < this.sections.length; i++) {
+           var section = this.sections[i];
+           route += section.departure.station.name + '&mdash;';
+           if (section.journey) {
+             route += '<em>' + section.journey.name.trim() + '</em>';
+           } else {
+             route += '<em>Walk</em>';
+           }
+           route += '&rarr;';
+           if (i == this.sections.length - 1) {
+             route += section.arrival.station.name.trim();
+           }
+         }
+         var line = '<tr><td>' +
+         departure + '</td><td>' +
+         route + '</td><td>' +
+         arrival + '</td></tr>';
+
+         $(id).append(line);
+       });
+       }, 'json');
+  }
   function normalize(id) {
     return id.replace(/[^a-zA-Z0-9]/g, '_');
   }
   function refresh() {
     $('#clock').text(moment().format('H:mm'));
-    for (var station in config.stations) {
-      get_timetable(station);
+    for (var i = 0; i < config.stations.length; i++) {
+      get_timetable(config.stations[i].station, config.stations[i].limit);
+    }
+    for (var i = 0; i < config.routes.length; i++) {
+      get_route(config.routes[i].von, config.routes[i].nach);
     }
   }
-  for (var station in config.stations) {
+  // Station boards
+  for (var i = 0; i < config.stations.length; i++) {
+    var station = config.stations[i].station;
     var table = '<div id="' + normalize(station) + '">' +
       '<h3 class="station">' + station + '</h3>' +
       '<table>' +
@@ -68,6 +118,23 @@ $(function() {
       '<tbody></tbody></table></div>';
     $('#stationboard').append(table);
   }
+  // Routes
+  for (var i = 0; i < config.routes.length; i++) {
+    var route = config.routes[i];
+    var id = normalize(route.von) + '-' + normalize(route.nach);
+    var table = '<div id="' + id + '">' +
+      '<h3 class="station">' + route.von + ' &rarr; ' + route.nach + '</h3>' +
+      '<table>' +
+      '<colgroup><col width="150"><col width="500"><col width="150"></colgroup>' +
+      '<thead><tr><th align="left">Abfahrtszeit</th>' +
+      '<th align="left">Route</th>' +
+      '<th align="left">Ankunftszeit</th>' +
+      '</tr></thead>' +
+      '<tbody></tbody></table></div>';
+    $('#routes').append(table);
+
+  }
+
   setInterval(refresh, 30000);
   refresh();
 });
